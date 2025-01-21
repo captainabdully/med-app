59e51f2306fde069c02d062fa0a0831812be1c53
Abdul <abdulahmad0305@gmail.com>
2025-01-21 19:59:05 +0300

    Initial Commit

---
+++ med-app/med_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  med_appApp.swift
+//  med-app
+//
+//  Created by Abdully on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct med_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
